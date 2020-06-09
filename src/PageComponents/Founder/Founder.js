b0c383db0f4cd553962bb250624c5a39753acfbc
w0utje36 <34859081+w0utje36@users.noreply.github.com>
2020-06-09 14:32:41 +0200

    Add files via upload

--- src/PageComponents/Founder/Founder.js
+++ src/PageComponents/Founder/Founder.js
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from "react";
+import { Image } from "react-bootstrap";
 
 export default function Founder() {
-    return (
-        <div>
-            
+  return (
+    <div>
+      <div class="grid-container">
+        <div class="founderText">
+          <h2>Nicklas Ericksen</h2>
+          <h3>Founder and Ceo</h3>
+          <p>
+            Lorem ipsum dolor sit amet,{" "}
+            <strong>consectetur adipiscing elit</strong>. Aliquam eget sapien
+            sapien. Curabitur in metus urna. In hac habitasse platea dictumst.
+            Phasellus eu sem sapien, sed vestibulum velit. Nam purus nibh,
+            lacinia non faucibus et, pharetra in dolor. Sed iaculis posuere diam
+            ut cursus.{" "}
+          </p>
         </div>
-    )
+        <div class="founderImg">
+          <Image
+            id="founderImg"
+            src="https://www.ludoviccareme.com/files/image_211_image_fr.jpg"
+          ></Image>
+        </div>
+      </div>
+    </div>
+  );
 }
