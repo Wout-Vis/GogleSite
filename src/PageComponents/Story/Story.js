d72dd337f2f2f1be61f331973b81af3efc79a0a3
w0utje36 <34859081+w0utje36@users.noreply.github.com>
2020-06-12 12:07:59 +0200

    Add files via upload

--- src/PageComponents/Story/Story.js
+++ src/PageComponents/Story/Story.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
+import { Image } from "react-bootstrap";
 
+import Timeline from "./timelineV2Asset.png";
 export default function Story() {
-    return (
-        <div>
-            
+  return (
+    <div>
+      <style>
+        @import
+        url('https://fonts.googleapis.com/css2?family=Roboto:wght@100&display=swap');
+      </style>
+      <div class="lt-grid-container">
+        <div class="lt-imgPlace">
+          <Image id="timeLine" src={Timeline}></Image>
         </div>
-    )
+        <div class="lt-txtPlace">
+          <h1 id="gogleStory">The full story of Gogle</h1>
+        </div>
+      </div>
+    </div>
+  );
 }
