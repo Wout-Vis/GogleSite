d92278d7ac92ff50173dacc546a76080a2c1139c
w0utje36 <34859081+w0utje36@users.noreply.github.com>
2020-06-15 11:43:40 +0200

    Add files via upload

--- src/PageComponents/Homepage/Homepage.js
+++ src/PageComponents/Homepage/Homepage.js
@@ -1,40 +1,49 @@
 import React from "react";
 import SlideShow from "./SlideShow";
 
-import { Image } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { Image, Card, Button } from "react-bootstrap";
 export default function Homepage() {
   return (
     <div>
       <SlideShow />
       <div className="row" style={{ padding: 40 }}>
-        <div id="homepageImgBox">
-          <img
-            alt="Google Home"
+        <div className="col-md-6">
+          <Image
+            alt="Bootstrap Image Preview"
+            id="homeImg"
             src="https://i.pinimg.com/originals/e7/c3/fe/e7c3fe22594e57b4af69f5ab42644ac6.jpg"
-            id="homepageImg"
+            fluid
           />
         </div>
-        <div id="founderQuoteBox">
-          <p id="founderQuote">
+        <div className="col-md-4" id="quoteBox">
+          <p id="quote">
             “From day one our goal at Gogle is to contribute to the betterment
             of humanity. We believe that our advanced technologies provide a
             revolutionary new way to manage finance, government, science,
             environment and more. Let’s all work together on a better tomorrow.”
           </p>
-          <p id="founderCitation">
-            - Nicklas Ericksen, Founder & CEO
-          </p>
         </div>
-        <div id="blogBox" >
-          <p>
-            To continue the “Meet a director” event, we would like to introduce you 
-            to this month's member: David Escudero. We know David as our Senior Vice 
-            President of the Gogle Science branch but he is much more than that! For 
-            example, David is what is known as a “trekkie”, a Star Trek fan. He is 
-            known around the office for his Star Trek mugs and his favourite character 
-            is Spock! David also has a dog named Chewie, quite a name for a Star Trek fan...
-          </p>
-        </div>
+      </div>
+      <div id="meetTheBoard">
+        <Card>
+          <Card.Header>Meet a director</Card.Header>
+          <Card.Body>
+            <Card.Title>Davis Escudero</Card.Title>
+            <Card.Text>
+              To continue the “Meet a director” event, we would like to
+              introduce you to this month's member: David Escudero. We know
+              David as our Senior Vice President of the Gogle Science branch but
+              he is much more than that! For example, David is what is known as
+              a “trekkie”, a Star Trek fan. He is known around the office for
+              his Star Trek mugs and his favourite character is Spock! David
+              also has a dog named Chewie, quite a name for a Star Trek fan...
+            </Card.Text>
+            <Button variant="primary" as={Link} to="/board">
+              Go somewhere
+            </Button>
+          </Card.Body>
+        </Card>
       </div>
     </div>
   );
