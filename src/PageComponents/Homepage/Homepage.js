3252663609c8e5645ce93b824ee6c577b55edefc
w0utje36 <34859081+w0utje36@users.noreply.github.com>
2020-06-15 10:11:39 +0200

    Add files via upload

--- src/PageComponents/Homepage/Homepage.js
+++ src/PageComponents/Homepage/Homepage.js
@@ -1,21 +1,22 @@
 import React from "react";
 import SlideShow from "./SlideShow";
 
-import { Image } from "react-bootstrap";
+import { Image, Toast } from "react-bootstrap";
 export default function Homepage() {
   return (
     <div>
       <SlideShow />
       <div className="row" style={{ padding: 40 }}>
-        <div className="col-md-8">
+        <div className="col-md-6">
           <Image
             alt="Bootstrap Image Preview"
+            id="homeImg"
             src="https://i.pinimg.com/originals/e7/c3/fe/e7c3fe22594e57b4af69f5ab42644ac6.jpg"
             fluid
           />
         </div>
-        <div className="col-md-4">
-          <p>
+        <div className="col-md-4" id="quoteBox">
+          <p id="quote">
             “From day one our goal at Gogle is to contribute to the betterment
             of humanity. We believe that our advanced technologies provide a
             revolutionary new way to manage finance, government, science,
