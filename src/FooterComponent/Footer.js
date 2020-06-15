9e7786dcc9a0e6b1c3e65b5e66b59813784baed5
ComittedComitter <65293328+ComittedComitter@users.noreply.github.com>
2020-06-15 13:42:17 +0200

    Add files via upload

--- src/FooterComponent/Footer.js
+++ src/FooterComponent/Footer.js
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from "react";
+import Container from "react-bootstrap/Container";
 
-export default function Footer() {
-    return (
-        <div>
-            
+const FooterPage = () => {
+  return (
+    <div class="container bg-light text-secondary mw-100">
+      <div class="row w-100 pl-4 pt-3">
+        <div class="col-sm-6 ">
+          <p>Netherlands</p>
         </div>
-    )
-}
+      </div>
+      <div class="row pl-4">
+        <div class="col ">
+          <p>Contact</p>
+        </div>
+        <div class="col ">
+          <p>Terms</p>
+        </div>
+        <div class="col ">
+          <p>Privacy</p>
+        </div>
+        <div class="col w-60">
+          <p></p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FooterPage;
