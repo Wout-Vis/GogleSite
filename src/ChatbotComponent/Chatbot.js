7d2900c6f1a69f926c7575b8c1c8995dc993bda3
w0utje36 <34859081+w0utje36@users.noreply.github.com>
2020-06-15 10:13:04 +0200

    Add files via upload

--- src/ChatbotComponent/Chatbot.js
+++ src/ChatbotComponent/Chatbot.js
@@ -1,13 +1,12 @@
-import React from 'react'
+import React from "react";
 import ChatbotComponent from "./ChatbotComponent";
 
 export default function Chatbot() {
+  function botEventHandler() {}
 
-    function botEventHandler() {}
-
-    return (
-        <div>
-            <ChatbotComponent eventHandler={botEventHandler} />
-        </div>
-    )
+  return (
+    <div>
+      <ChatbotComponent eventHandler={botEventHandler} />
+    </div>
+  );
 }
