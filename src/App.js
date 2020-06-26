6d302146726101e4222de4e95d968084e832919f
w0utje36 <34859081+w0utje36@users.noreply.github.com>
2020-06-26 15:16:32 +0200

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -10,7 +10,6 @@ import Footer from "./FooterComponent/Footer";
 import Chatbot from "./ChatbotComponent/Chatbot";
 
 import Progress from "./Progress";
-
 // Page Imports
 import Homepage from "./PageComponents/Homepage/Homepage";
 import Products from "./PageComponents/Branches/Branches";
@@ -18,71 +17,94 @@ import Founder from "./PageComponents/Founder/Founder";
 import Board from "./PageComponents/Board/Board";
 import Story from "./PageComponents/Story/Story";
 
-import DotPuzzle from './Puzzles/DotPuzzle';
-import RotatePuzzle from './Puzzles/RotatePuzzle'
+// After login page imports
+import Integrety from "./PageComponents/CodeOfConduct/Integrety";
+import PrePannel from "./PageComponents/PrePannel/PrePannel";
+import NiceReads from "./PageComponents/NiceReads/NiceReads";
+import Profile from "./PageComponents/ProfilePage/Profile";
+import ProfileInformation from "./PageComponents/ProfileInformation/ProfileInformation";
 
-import MockUp from './ControlPanel/MockUp';
+import DotPuzzle from "./Puzzles/DotPuzzle";
 
+import MockUp from "./ControlPanel/MockUp";
+import FooterPage from "./FooterComponent/Footer";
+import Header2 from "./HeaderComponent/Header2";
 export default class App extends Component {
-  
-  constructor()
-  {
+  constructor() {
     super();
-    Progress.updateProgress('0');
+    Progress.updateProgress("0");
     this.state = {
-      progress: Progress.getProgress()
+      progress: Progress.getProgress(),
     };
     this.introTimer = 0;
   }
 
   componentDidMount() {
-    this.progressTicker = setInterval(
-      () => this.tick(), 500
-    );
+    this.progressTicker = setInterval(() => this.tick(), 500);
   }
 
   componentWillUnmount() {
-    clearInterval( this.progressTicker );
+    clearInterval(this.progressTicker);
   }
 
   tick() {
-    if ( this.introTimer > 10 )
-    {
-      this.setState(
-        { progress: Progress.getProgress() }
-      );
+    if (this.introTimer > 10) {
+      this.setState({ progress: Progress.getProgress() });
     } else {
       this.introTimer++;
     }
   }
 
   render() {
-
-    switch ( this.state.progress )
-    {
-      case '4':
-        return(
-          <>
-            End Screen
-          </>
-        );
-      case '3':
+    switch (this.state.progress) {
+      case "5":
+        return <>End Screen</>;
+      case "4":
         return (
           <>
             <MockUp />
           </>
         );
-      case '2':
-          return (
-            <>
-              <div id="puzzleBG">
-                <div id="puzzleBox">
-                  <DotPuzzle />
-                  <RotatePuzzle />
-                </div>
+      case "3":
+        return (
+          <>
+            <Router>
+              <Header2 />
+              <Switch>
+                <Route path="/Qin3M3n09nDhJHU2nuhdD" component={Profile} />
+                <Route
+                  path="/3a(3kJe28nkhdh703hkh3mpl0ye"
+                  component={Integrety}
+                />
+                <Route
+                  name="Books"
+                  path="/jnD3ejLpEnmZW9789Hnl83lDmHQE"
+                  component={NiceReads}
+                />
+                <Route
+                  name="prePanel"
+                  path="/JSn6yFsTxCRne2uOYCtt"
+                  component={PrePannel}
+                />
+                <Route
+                  name="story"
+                  path="/fdsHD8H3jDeqN082"
+                  component={ProfileInformation}
+                />
+              </Switch>
+            </Router>
+          </>
+        );
+      case "2":
+        return (
+          <>
+            <div id="puzzleBG">
+              <div id="puzzleBox">
+                <DotPuzzle />
               </div>
-            </>
-          );
+            </div>
+          </>
+        );
       default:
         return (
           <>
@@ -97,12 +119,12 @@ export default class App extends Component {
                 <Route name="story" path="/story" component={Story} />
               </Switch>
             </Router>
-
-            <Footer />
+            <Profile />
+            <FooterPage />
 
             <IntroScene />
           </>
         );
-      }
+    }
   }
 }
