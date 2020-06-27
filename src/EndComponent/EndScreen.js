bdd8e09cca748950df6385c3fd95f0342cce2cac
ZJVieth <52859000+ZJVieth@users.noreply.github.com>
2020-06-27 11:42:50 +0200

    Add files via upload

---
+++ src/EndComponent/EndScreen.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function EndScreen() {
+    return (
+        <div>
+            <div id="endBox">
+                <h1>The End</h1>
+                <div id="introTextBox" >
+                    by Double Quantum Quartet
+                </div>
+            </div>
+        </div>
+    )
+}
